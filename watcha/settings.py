042d7fa9d444e72546e68881c85b7789b8921a0d
dmlimgo <dongmyeong.lim@gmail.com>
2019-05-12 22:29:27 +0900

    urls, 하얀 화면 출력 완료

--- watcha/settings.py
+++ watcha/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'watcha.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'watcha', 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
