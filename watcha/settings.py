ccb33ec487fd40ba576b03116a218f062258824e
dmlimgo <dongmyeong.lim@gmail.com>
2019-05-16 20:02:24 +0900

    last... please...

--- watcha/settings.py
+++ watcha/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'crispy_forms',
     'accounts.apps.AccountsConfig',
     'rest_framework_swagger',
     'rest_framework',
